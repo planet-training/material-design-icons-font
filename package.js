b965d721a19f5330623c71607d97441c9c5a7245
Max Bruchmann <maxbruchmann@gmail.com>
2015-11-24 14:41:25 +0100

    fixed css adding2

--- package.js
+++ package.js
@@ -1,19 +1,20 @@
 Package.describe({
   summary: "Material design icons from google (font)",
   name:"planettraining:material-design-icons-font",
-  version: "2.1.1",
+  version: "2.1.1_1",
   git:"https://github.com/planet-training/material-design-icons-font.git"
 });
 
 Package.onUse(function (api) {
   api.versionsFrom('1.0');
+  api.addAssets(['design-icons.css'],'client');
   api.addAssets([
     /* icon sprites */
     'bower_components/material-design-icons/iconfont/MaterialIcons-Regular.eot',
     'bower_components/material-design-icons/iconfont/MaterialIcons-Regular.ttf',
     'bower_components/material-design-icons/iconfont/MaterialIcons-Regular.woff',
-    'bower_components/material-design-icons/iconfont/MaterialIcons-Regular.woff2',
+    'bower_components/material-design-icons/iconfont/MaterialIcons-Regular.woff2'
 
-    'design-icons.css'
+
   ], "client");
 });
